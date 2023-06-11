63f1d19f39c0bbeb4c54e991eddecf828b887719
Kauafs <135078086+Kauafs@users.noreply.github.com>
2023-06-11 20:25:10 -0300

    Add files via upload

---
+++ Python-Felipe/functions/produto_db.py
@@ -0,0 +1,42 @@
+def cadastro_produto(conexao,nome,qtd,marca):
+    cursor = conexao.cursor()
+
+    sql = f'INSERT INTO produto (produto_nome,produto_qtd,produto_marca) VALUES (?,?,?)'
+    cursor.execute(sql,[nome,qtd,marca])
+    conexao.commit()
+    return True
+
+def atualizar_produto(conexao,novo_produto,id):
+    sql = 'UPDATE produto SET produto_nome=? WHERE produto_id=?'
+    cursor = conexao.cursor()
+    cursor.execute(sql,[id,novo_produto])
+    conexao.commit()
+
+def deletar_produto(conexao,id):
+    sql = 'DELETE FROM produto WHERE produto_id=?'
+    cursor = conexao.cursor()
+    cursor.execute (sql, [id])
+    conexao.commit() 
+
+def listar_produto(conexao):
+    sql = 'SELECT*FROM produto'
+    cursor = conexao.cursor()
+    cursor.execute(sql)
+    return  cursor.fetchall()
+
+def buscar1_produto(conn,busca_name):
+    sql = 'select*from produto WHERE produto_nome LIKE ? '
+    cursor = conn.cursor()
+    cursor.execute(sql,[f'%{busca_name}%'])
+    produto = cursor.fetchall()
+    print(produto)
+
+def venda_produto(conexao,venda,nick_produto,id_produto,quantidade):
+    
+    cursor = conexao.cursor()
+    sql = f'INSERT INTO venda (venda_data,produto_id,clientes_id,venda_qtd) VALUES (?,?,?,?)'
+    cursor.execute(sql,[venda,nick_produto,id_produto,quantidade])
+    conexao.commit()
+    return True
+    
+
