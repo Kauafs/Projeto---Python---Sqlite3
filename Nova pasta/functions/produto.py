7d7ef90c93efec7dc8032b24ebf4d3d926a74113
Kauafs <135078086+Kauafs@users.noreply.github.com>
2023-06-13 12:55:24 -0300

    Delete Nova pasta directory

--- Nova pasta/functions/produto.py
+++
@@ -1,8 +0,0 @@
-def cadastrar_produto(conexao, produto):
-        cursor = conexao.cursor()
-
-        sql = f'INSERT INTO produtos (produtos_name) VALUES (?)'
-        cursor.execute(sql, [produto])
-        conexao.commit()
-    
-        return True
