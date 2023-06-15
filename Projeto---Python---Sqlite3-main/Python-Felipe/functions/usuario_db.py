02b4f34003ec349e070a6ed83d5edc5abeeee167
Kauafs <135078086+Kauafs@users.noreply.github.com>
2023-06-15 15:41:21 -0300

    Delete Projeto---Python---Sqlite3-main directory

--- Projeto---Python---Sqlite3-main/Python-Felipe/functions/usuario_db.py
+++
@@ -1,8 +0,0 @@
-def login_usuario(conexao, email, senha):
-    cursor = conexao.cursor()
-    sql = (
-        sql
-    ) = f"INSERT INTO login(login_email,login_password) VALUES (?,?)"
-    cursor.execute(sql, [email, senha])
-
-    return cursor.fetchall()
